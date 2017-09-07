5cd39146a706aadeb0a84317d8b15c2e7b535f49
s_selling <sebastian.selling@outlook.com>
2017-09-07 14:05:19 +0200

    Removed js files

--- js/custom.js
+++
@@ -1,9 +0,0 @@
-// accordion jquery ui menu
-  $( function() {
-    $( "#accordion" ).accordion({
-      collapsible: true,
-      heightStyle: "content"
-    });
-  } );
-// force hashlinks
-window.setHashLink = 1;
