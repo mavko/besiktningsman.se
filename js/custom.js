5e9c20731544fd37af4e240cd5dea6db5eb72b87
Sebastian Selling <sebastianselling@Sebastians-MacBook-Pro.local>
2019-01-22 12:20:08 +0100

    Branding page

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,14 @@
 $(function(){initAccordion($(".accordion"))});var initAccordion=function(i){i.each(function(){var i={heightStyle:"content",collapsible:!0,active:!1,animate:!0,beforeActivate:function(i,t){window.location.hash=t.newHeader.attr("id")||"/"}},t=$(this).attr("data-active-index");null!==t&&void 0!==t&&(i.active=Number(t));var n=decodeURIComponent(window.location.hash);$(this).find(">h3").each(function(t){this.id=$(this).text().toLowerCase().replace(/\s+/g,"_"),n&&n=="#"+this.id&&(i.active=t)}),$(this).accordion(i)})};window.disableKeyNavigation=0,window.setHashLink=1;
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  document.execCommand("copy");
+
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Kopierade: " + copyText.value;
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Kopiera till urklipp";
+}
